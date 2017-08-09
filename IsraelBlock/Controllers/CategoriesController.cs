fe05d60ad6937331b60c83d29e8d3588ce4c5b27
Israel Block <israelblock@gmail.com>
2017-08-08 22:16:06 -0300

    Nova Chamada para Printers

--- IsraelBlock/Controllers/CategoriesController.cs
+++ IsraelBlock/Controllers/CategoriesController.cs
@@ -15,7 +15,7 @@ namespace IsraelBlock.Controllers
             new Category(){ CategoryId = 2, Name = "Monitor"},
             new Category(){ CategoryId = 3, Name = "Notebook"},
             new Category(){ CategoryId = 4, Name = "Mouse"},
-            new Category(){ CategoryId = 5, Name = "Printer"}
+            new Category(){ CategoryId = 5, Name = "Printers"}
         };
 
         // GET: Categories

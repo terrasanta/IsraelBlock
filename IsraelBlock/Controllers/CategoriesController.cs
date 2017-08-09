7a747e9a5da57ae50b762e993032c7278750928b
Israel Block <israelblock@gmail.com>
2017-08-08 21:57:56 -0300

    Primeiro Commit do Projeto .Net MVC

---
+++ IsraelBlock/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IsraelBlock.Models;
+
+namespace IsraelBlock.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private static IList<Category> categoryList = new List<Category>()
+        {
+            new Category(){ CategoryId = 1, Name = "Keyboard"},
+            new Category(){ CategoryId = 2, Name = "Monitor"},
+            new Category(){ CategoryId = 3, Name = "Notebook"},
+            new Category(){ CategoryId = 4, Name = "Mouse"},
+            new Category(){ CategoryId = 5, Name = "Printer"}
+        };
+
+        // GET: Categories
+        public ActionResult Index()
+        {   
+            return View(categoryList.OrderBy(c => c.Name));
+        }
+    }
+}
---
+++ IsraelBlock/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IsraelBlock.Models
+{
+    public class Category
+    {
+        public long CategoryId { get; set; }
+        public string Name { get; set; }
+
+    }
+}
